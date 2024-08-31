4f7ba96a483e0613a9fafb6ad5c5154176742594
otavyo01 <108097346+otavyo01@users.noreply.github.com>
2024-08-31 17:11:54 -0300

    Add files via upload

---
+++ js/jscalc.js
@@ -0,0 +1,31 @@
+function soma(){
+    let n1 = document.getElementById("numero_um").value
+    let n2 = document.getElementById("numero_dois").value
+    let soma = parseInt(n1) + parseInt(n2)
+
+    document.getElementById("res").value = soma
+}
+
+function subtracao(){
+    let n1 = document.getElementById("numero_um").value
+    let n2 = document.getElementById("numero_dois").value
+    let soma = parseInt(n1) - parseInt(n2)
+
+    document.getElementById("res").value = soma
+}
+
+function multiplicacao(){
+    let n1 = document.getElementById("numero_um").value
+    let n2 = document.getElementById("numero_dois").value
+    let soma = parseInt(n1) * parseInt(n2)
+
+    document.getElementById("res").value = soma
+}
+
+function divisao(){
+    let n1 = document.getElementById("numero_um").value
+    let n2 = document.getElementById("numero_dois").value
+    let soma = parseInt(n1) - parseInt(n2)
+
+    document.getElementById("res").value = soma
+}
